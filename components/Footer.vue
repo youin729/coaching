38b184c837885e75bad1879992668fcee1746256
h.kasamatsu <you.in729@gmail.com>
2021-06-12 16:04:28 +0900

    vue/nuxt削除

--- components/Footer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <footer class="footer">
-    <div class="container-fluid">
-      <div class="copyright float-right">
-        &copy; Sakura Tech Inc.,
-      </div>
-    </div>
-  </footer>
-</template>
